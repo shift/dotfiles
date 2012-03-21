8cf012e589f4e8488575360a6d8fbd591cc5921b
Vincent Palmer <shift@someone.section.me>
2012-03-21 08:39:25 +0000

    Convert to git from old dropbox setup.

---
+++ README.md
@@ -0,0 +1,7 @@
+# shift dotfiles
+
+To install:
+
+    git clone http://github.com/shift/dotfiles.git && cd dotfiles && ./install.sh
+
+I use vimmer for handling my vim plugins. (gem install vimmer).
---
+++ install.sh
@@ -0,0 +1,26 @@
+#!/bin/env bash
+
+echo -n "Setting up: "
+
+echo -n "tmux.conf"
+mv ~/.tmux.conf ~/.tmux.conf.bak > /dev/null 2>&1
+ln -s $PWD/tmux.conf ~/.tmux.conf
+cp -a $PWD/tmux ~/.tmux
+echo -n ". "
+
+echo -n "shell-fm.conf"
+mkdir -p ~/.shell-fm
+mv ~/.shell-fm/shell-fm.rc ~/.shell-fm/shell-fm.rc.bak > /dev/null 2>&1
+ln -s $PWD/shell-fm.rc ~/.shell-fm/shell-fm.rc
+echo -n ". "
+
+echo -n "vimrc"
+mv ~/.vimrc ~/.vimrc.bak  > /dev/null 2>&1
+ln -s $PWD/vimrc ~/.vimrc
+echo -n ". "
+
+echo "vim-pathogen"
+mkdir -p ~/.vim/autoload ~/.vim/bundle
+curl -so ~/.vim/autoload/pathogen.vim  https://raw.github.com/tpope/vim-pathogen/HEAD/autoload/pathogen.vim
+echo ". "
+echo "Done"
